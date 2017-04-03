cf07a3b788dcb009633340ca2508790bed6a139f
David Chang <dchang103@gmail.com>
2017-04-02 21:19:28 -0700

    GraphWeightedPaths

---
+++ GraphWeightedPaths.js
@@ -0,0 +1,21 @@
+// 2) Given a undirected graph with weights, return the sum of the weight of each path between two nodes (shortest path between two vertices). Assume there are no cycles. 
+
+// Example:
+
+
+// Input:
+//        A
+//        | 1
+//        B
+//    2 /   \ 3
+//     C     D
+    
+// Output:
+// 18
+// since 
+// A to B has weight 1
+// A to C has weight 3
+// A to D has weight 4
+// B to C has weight 2
+// B to D has weight 3
+// C to D has weight 5
