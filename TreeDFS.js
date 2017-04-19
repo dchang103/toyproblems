73db483659ed4fded24bba1587f662806df00cc2
David Chang <dchang103@gmail.com>
2017-04-18 23:42:01 -0700

    TreeDFS.js

--- TreeDFS.js
+++ TreeDFS.js
@@ -1,16 +1,2 @@
 // 3) Write an iterative function to implement preorder traversal on a given binary tree
 // (note that recursion algorithm is not used here), and return the node values in an array.
-
-const preorderFlatten = (root) => {
-  var treeVals = []
-  
-  const dfTraversal = (node) => {
-    treeVals.push(node.value)
-    if (node.left) { dfTraversal(node.left) }
-    if (node.right) { dfTraversal(node.right) }
-  }
-
-  dfTraversal(root)
-
-  return treeVals
-}
