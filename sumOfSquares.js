386604211cc1eefd793171925897085dc74c337f
David Chang <dchang103@gmail.com>
2017-04-20 23:50:46 -0700

    sumOfSquares.js

---
+++ sumOfSquares.js
@@ -0,0 +1,10 @@
+// Using any language of your choice, write a function that takes an integer 
+// argument, and returns true if the integer is the sum of the squares of two 
+// integers, and false otherwise. (Note that the integer 0 doesn't count).
+// Example: f(34) = true because 34 = 3*3 + 5*5; f(35) = false
+
+// Hint:
+// brute force: O(n^2)
+// optimal: O(n) 
+// Can't it be done in Math.sqrt(n)?
+// Jack: To be accurate - O( Math.sqrt(n) ), which is still 'rounded' to O(n)
